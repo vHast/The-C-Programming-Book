c98a0d45f50912c3dfbecc0106ec5e7675c40d26
vHast <fedvhast@gmail.com>
2026-09-07 00:00:23 +0300

    First exercise

---
+++ Exercises/Chapter_One/ExerciseOne.c
@@ -0,0 +1,7 @@
+#include <stdio.h> // This will include the stdio.h library
+
+int main() // Main will be the main function that will call the other ones, by C98 standard, we have to define that it will return an int value
+{
+    printf("Hello, world\n"); // Printf will print the output, following by the newline
+}
+ 
